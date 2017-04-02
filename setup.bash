26297aadad1185cf5c63d137031a0b4b6237e874
Nygel Lopez <nygel.lopez@mail.utoronto.ca>
2017-04-02 19:05:22 -0400

    fixing setup file

--- setup.bash
+++ setup.bash
@@ -1,12 +1,10 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+read -p "Enter port for API: " apiport
+read -p "Enter port for game: " gameport
 read -p "Enter database name: " dbname
 read -p "Enter UTORid: " utorid
 read -s -p "Enter password: " password
-read -p "Enter port for API: " apiport
-read -p "Enter port for game: " gameport
-
-echo -e "\n"
 
 cd "$parent_path"
 sed -i "s/dbnamehere/$dbname/g" index.js
@@ -14,3 +12,4 @@ sed -i "s/userhere/$utorid/g" index.js
 sed -i "s/passwordhere/$password/g" index.js
 sed -i "s/apiporthere/$apiport/g" index.js
 sed -i "s/gameporthere/$gameport/g" controller.js
+sed -i "s/gameporthere/$gameport/g" ww.js
